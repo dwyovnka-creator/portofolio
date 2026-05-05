941b46565a2099f793280a0bec1a082caf27804a
Dewi Wuryan <dwyovnka@gmail.com>
2026-05-05 09:43:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
